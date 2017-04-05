0007005a9c943905f8a554f8660e814e16859549
Sistla, Sunil Kumar <sunilkumar.sistla@cdk.com>
2017-04-05 12:57:25 +0530

    optimized poisonous plants with stack

--- js/hackerrank-poision-plants.js
+++ js/hackerrank-poision-plants.js
@@ -1,5 +1,13 @@
 // https://www.hackerrank.com/challenges/poisonous-plants
 
+function max(a, b) {
+    return (a > b) ? a : b;
+}
+
+function min(a, b) {
+    return (a < b) ? a : b;
+}
+
 function node(a) {
     var self = this;
     self.data = a;
@@ -16,7 +24,7 @@ function node(a) {
     }
 }
 
-function processData(input) {
+function greedy(input) {
     var i, root = null, current = null, pre = null, p1 = input.split("\n")[1].split(" ").map(Number), clr = false;
     for(i=p1.length-1;i>=0;--i) {
         if(current === null) {
@@ -51,3 +59,31 @@ function processData(input) {
     
     console.log(--d);
 } 
+
+function optimized(input) {
+    input = input.split("\n").map(function(a){ return a.split(" ").map(Number);});
+    var len = input[0][0]; pest = input[1];
+    var maxD = 0, currD = 0, stk = [], minP = pest[0], currP = pest[0];
+        
+    for(var i=0;i<len;i++) {
+        currD = 0;
+        while(stk.length > 0 && pest[i] <= stk[stk.length-1].p) {
+            currD = max(currD, stk.pop().d);
+        }
+        if(stk.length == 0) currD = 0; else currD++;
+        maxD = max(currD, maxD);
+        stk.push({d: currD, p: pest[i]});
+    }
+    console.log(maxD);
+} 
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   optimized(_input);
+});
